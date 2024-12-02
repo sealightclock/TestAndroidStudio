5e09555931fd8067e738ebb4baa2bd28779b3ad9
Sealight Clock <chbebram@hotmail.com>
2024-12-01 22:31:37 -0800

    Screens with Column: Optimize alignment.

--- app/src/main/java/com/example/jonathan/testandroidstudio/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/jonathan/testandroidstudio/view/WelcomeScreen.kt
@@ -1,20 +1,37 @@
 package com.example.jonathan.testandroidstudio.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun WelcomeScreen(navController: NavHostController) {
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
         Text(
             text = "Welcome to TestAndroidStudio"
         )
 
-        Button(onClick = { navController.navigate("next") }) {
-            Text("Go to next screen")
+        Button(
+            onClick = { navController.navigate("next") })
+        {
+            Text(
+                text = "Go to next screen"
+            )
         }
     }
 }
